b625532f640259128ab41e5c745a54f38514186f
S1Acharya123 <saketacharya.08@gmail.com>
2021-06-06 14:26:24 +0530

    Add data to firebase dynamically

--- src/App.js
+++ src/App.js
@@ -1,15 +1,65 @@
 import './App.css';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import { useState } from 'react';
+// import Button from '@material-ui/core/Button';
+import {db} from "./firebase_config";
+import firebase from "firebase";
 
 function App() {
+
+  // we save the data that user types to a variable , so that we can later push it to database
+  // for this we will use hooks
+
+  // here state is the variable and setstate is the function which updates the variable.
+  // this state can't be updated without this function
+  // initialState is actually th initial value for texField
+  // const [state, setState] = useState(initialState);
+  const [todoInput , setTodoInput] = useState(" ");
+
+  // add todo to database
+  // this works whenever user hits enter after writing text
+  function addtodo(e) {
+    e.preventDefault();         //this is to stop the page from reloading
+    // console.log("add a todo");
+    db
+    .collection("todos")
+    .add({
+      inprogress: true,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      todo:todoInput,
+    })
+// next line is so that after we add a new todo to database , we get an empty textfield to type again
+    setTodoInput("");
+
+  };
+
+  // we can also write above function this way
+  // const addtodo = () => {
+  // }
+
   return (
     <div className="App" style={styles.textField}>
       <Button variant="contained" color="primary">
         <h1>Let's start the show Babes !</h1>
       </Button>
       <p></p>
-      <TextField id="filled-basic" label="Write a to-do" variant="filled" style={{maxWidth: "300px" , width : "90vw"}}/>
+      <div>
+          <form>
+            <TextField 
+              id="standard-basic" 
+              label="Write a to-do"
+              value = {todoInput}
+              onChange = {(e) => {
+                setTodoInput(e.target.value);
+                // console.log('this is the todo input' , e.target.value);
+                // console.log(`this is the todo input ${e.target.value}`);  // another way of writing console using backtick
+              }}
+              style={{maxWidth: "300px" , width : "90vw"}}
+            />
+            <Button type = "submit" variant="contained" onClick = {addtodo} style={styles.button}>Okay!</Button>
+          </form>
+      </div>
     </div>
   );
 }
@@ -25,6 +75,9 @@ const styles = {
     justifyContent: "center",
     alignItems: "center",
   },
+  // button: {
+  //   display: "none",
+  // },
   nav: {
     height: 70,
     background: '#4267b2',
